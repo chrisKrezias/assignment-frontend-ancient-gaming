85081adc8e5dec4ce17cca048ba202074a91e6d4
Christos Krezias <ckrezias3@gatech.edu>
2022-07-17 05:55:14 +0300

    Fix build issues and update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'boxes', loadChildren: () => import('./boxes/boxes.module').then(m => m.BoxesModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./boxes/boxes.module').then(m => m.BoxesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
